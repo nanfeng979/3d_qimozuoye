bcd9b8c7ee0133897353614542e032b6df15dc04
nanfeng979 <306338401@qq.com>
2023-05-25 23:43:55 +0800

    添加游戏时间倒计时

---
+++ Components/GameTimeCountDown/GameTimeCountDown.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameTimeCountDown : MonoBehaviour
+{
+    public float GameOverTime;
+
+    [SerializeField] private TMP_Text GameTimeText;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R)) {
+            // 重置游戏关卡
+            ResetGame();
+        }
+
+        GameOverTime -= Time.deltaTime;
+
+        if(GameOverTime <= 0.0f) {
+            GameOver();
+            return;
+        }
+
+        // 实时更新倒计时
+        GameTimeText.text = "距离游戏结束还剩下：" + GameOverTime.ToString("F6") + " 秒";
+    }
+
+    private void GameOver() {
+        GameTimeText.text = "游戏结束";
+
+        // 暂停时间
+        Time.timeScale = 0.0f;
+    }
+
+    private void ResetGame() {
+        SceneManager.LoadScene("Playing");
+    }
+}
