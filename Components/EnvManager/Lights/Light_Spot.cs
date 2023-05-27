26f6f24ad4d2f3d367ebd2d160d15ec087dd7d5d
nanfeng979 <306338401@qq.com>
2023-05-27 16:10:11 +0800

    给灯光识别玩家经过的脚本

--- Components/EnvManager/Lights/Light_Spot.cs
+++ Components/EnvManager/Lights/Light_Spot.cs
@@ -5,19 +5,42 @@ using UnityEngine;
 public class Light_Spot : MonoBehaviour
 {
     public GameObject Player;
+    private Vector3 PlayerOldPos;
+    private Vector3 PlayerNewPos;
+    
     private Light _light;
+
+    private bool isLeft;
 
     void Start()
     {
+        PlayerOldPos = Player.transform.position;
+
         _light = GetComponent<Light>();
+
+        if(gameObject.name == "Left") {
+            isLeft = true;
+        } else {
+            isLeft = false;
+        }
+
+        _light.intensity = 0.0f;
     }
 
     void Update()
     {
-        if(Player.transform.position.z >= transform.position.z - 2) {
-            _light.enabled = true;
-        } else {
-            _light.enabled = false;
+        PlayerNewPos = Player.transform.position;
+
+
+        if(Vector3.Distance(Player.transform.position, transform.position) > 8.0f) {
+            return;
         }
+
+        if(Mathf.Abs((Player.transform.forward - transform.forward).x) < 0.5f) {
+            _light.intensity = 2.5f;
+        }
+        
+
+        PlayerOldPos = PlayerNewPos;
     }
 }
