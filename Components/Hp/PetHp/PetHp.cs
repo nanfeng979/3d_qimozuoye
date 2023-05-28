b863eb1892940d2d419465de7cd8753aa97a2521
nanfeng979 <306338401@qq.com>
2023-05-28 19:44:25 +0800

    新建一个PetHp文件，等待一个角色类

---
+++ Components/Hp/PetHp/PetHp.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PetHp : MonoBehaviour
+{
+    public int Hp;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
